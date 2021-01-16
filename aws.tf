83d0076720ce1b564fd7a5ee6b57d6e630e1a396
PatrickRobotham <patrick.robotham2@gmail.com>
2021-01-16 17:41:33 +1100

    Add user authentication

--- aws.tf
+++ aws.tf
@@ -2,3 +2,13 @@ provider "aws" {
     profile = "default"
     region = "ap-southeast-2"
 }
+// Website runs on amplify. no provider module yet :(
+
+resource "aws_cognito_user_pool" "pool" {
+  name = "WyldRydes"
+}
+
+resource "aws_cognito_user_pool_client" "client" {
+    name = "WyldRydesWebApp"
+    user_pool_id = aws_cognito_user_pool.pool.id
+}
