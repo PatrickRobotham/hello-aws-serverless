e42549f3b53701126c9401cb8f19e00540fec328
PatrickRobotham <patrick.robotham2@gmail.com>
2021-01-17 17:49:01 +1100

    Change user pool

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-2_zzZak4eMs', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3iutksvhn94la9ikrrem29kjbm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-southeast-2_nK1pXXJkY', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '58ppvtjnav6b77eqo4vvic1c3n', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
